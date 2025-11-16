bacb65881a8d505eebac9c308cd06c2114d9cc9a
ChoiLeo99 <choileo0910@gmail.com>
2025-11-16 16:06:23 +0900

    프로그래머스 Lv0 나머지 구하기 문제 풀이

---
+++ Lv0/나머지 구하기.js
@@ -0,0 +1,20 @@
+// 문제
+// 정수 num1, num2 가 매개변수로 주어질 때,
+// num1 을 num2 로 나눈 나머지를 return
+
+function solution(num1, num2) {
+    var answer = -1;
+    return answer;
+}
+
+// 내가 쓴 코드
+function solution(num1, num2) {
+    var answer = num1 % num2;
+    return answer;
+}
+
+// 풀이
+// num1 을 num2 로 나누고 나머지를 가져오는 % 를 사용
+
+// 참고
+// % (나머지만 가져오는 연산자)
