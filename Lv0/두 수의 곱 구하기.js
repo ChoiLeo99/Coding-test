744566fb3518d550322a752b05036346951eecc1
ChoiLeo99 <choileo0910@gmail.com>
2025-11-17 21:07:47 +0900

    프로그래머스 Lv0 두 수의 곱 구하기 문제 풀이

---
+++ Lv0/두 수의 곱 구하기.js
@@ -0,0 +1,18 @@
+// 문제
+// 정수 num1 과 num2 가 매개변수로 주어질 때,
+// num1 과 num2 를 곱한 값을 return
+
+function solution(num1, num2) {
+   var answer = 0;
+   return answer;
+}
+
+// 내가 쓴 코드
+function solution(num1, num2) {
+   var answer = num1 * num2;
+   return answer;
+}
+
+// 풀이
+// 정수 num1 과 num2 를 * (곱 연산자) 를 사용해
+// answer 에 값을 담고 return 한다
