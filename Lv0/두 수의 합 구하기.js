189b9fb4f9066fc0376e5097e3a5c6a8d4a075cd
ChoiLeo99 <choileo0910@gmail.com>
2025-11-17 23:18:35 +0900

    프로그래머스 Lv0 두 수의 합 구하기 문제 풀이

---
+++ Lv0/두 수의 합 구하기.js
@@ -0,0 +1,18 @@
+// 문제
+// 정수 num1 과 num2 가 주어질 때,
+// num1 과 num2 를 더한 값을 return
+
+function solution(num1, num2) {
+   var answer = -1;
+   return answer;
+}
+
+// 내가 쓴 코드
+function solution(num1, num2) {
+   var answer = num1 + num2;
+   return answer;
+}
+
+// 풀이
+// 정수 num1 과 num2 를 더한 값을 answer에 담고
+// 그 answer를 return 한다
