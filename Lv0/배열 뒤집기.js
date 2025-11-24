ec1c497adea24a72e8e14ae12b5c6cafe2ecba64
ChoiLeo99 <choileo0910@gmail.com>
2025-11-24 23:11:54 +0900

    프로그래머스 Lv0 배열 뒤집기 문제 풀이

---
+++ Lv0/배열 뒤집기.js
@@ -0,0 +1,20 @@
+// 문제
+// 정수가 들어 있는 배열 num_list 가 매개변수로 주어집니다.
+// num_list 의 원소의 순서를 거꾸로 뒤집은 배열을 return
+
+function solution(num_list) {
+   var answer = [];
+   return answer;
+}
+
+// 내가 쓴 풀이
+function solution(num_list) {
+   return num_list.reverse();
+}
+
+// 풀이
+// num_list 의 배열을 reverse()를 사용해 거꾸로 뒤집어서 return 한다
+
+// 참고
+// reverse() - JavaScript 배열의 내장 함수
+// 배열의 순서를 거꾸로 뒤집는 기능을 한다
