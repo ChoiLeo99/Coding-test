f0eec358fbd8f945ba90fd1dc2f949564917f9c5
ChoiLeo99 <choileo0910@gmail.com>
2026-01-30 22:58:01 +0900

    프로그래머스 Lv0 짝수 홀수 개수 문제 풀이 수정

--- Lv0/짝수 홀수 개수.js
+++ Lv0/짝수 홀수 개수.js
@@ -9,16 +9,11 @@ function solution(num_list) {
 
 // 내가 쓴 풀이
 function solution(num_list) {
-   let even = 0;
-   let odd = 0;
-
-   for (let num of num_list) {
-      if (num % 2 === 0) {
-         even++;
-      } else {
-         odd++;
-      }
-   }
-
-   return [even, odd];
+   return [num_list.filter(n => n % 2 === 0).length, num_list.filter(n => n % 2 === 1).length];
 }
+
+// 풀이
+// filter -> 조건을 만족하는 요소만 골라서 새로운 배열을 만들어주는 함수
+// num_list.filter(n => n % 2 === 0).length -> 짝수만 모아서 배열로 반환
+// num_list.filter(n => n % 2 === 1).length -> 홀수만 모아서 배열로 반환
+//  .length 는 짝수, 홀수 개수
