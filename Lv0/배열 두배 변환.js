4f5352c9d90e30fc4701b4279e7e3132e11e7f66
ChoiLeo99 <choileo0910@gmail.com>
2026-02-05 22:35:17 +0900

    프로그래머스 Lv0 배열 두배 변환 문제 풀이

---
+++ Lv0/배열 두배 변환.js
@@ -0,0 +1,17 @@
+// 문제
+// 정수 배열 numbers 가 매개변수로 주어질 때
+// numbers 의 각 원소에 두배한 원소를 가진 배열을 return
+
+function solution(numbers) {
+   var answer = [];
+   return answer;
+}
+
+// 내가 쓴 풀이
+function solution(numbers) {
+   return numbers.map(n => n * 2);
+}
+
+// 풀이
+// numbers 의 배열의 각 요소를 하나씩 꺼내서
+// *2 를 해주고 그 값을 return 한다
