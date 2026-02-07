4a4a2351fc35191ca7668e04b01150c85783da08
ChoiLeo99 <choileo0910@gmail.com>
2026-02-07 23:57:49 +0900

    프로그래머스 Lv0 문자 리스트를 문자열로 반환하기 문제 풀이

---
+++ Lv0/문자 리스트를 문자열로 변환하기.js
@@ -0,0 +1,16 @@
+// 문제
+// 문자열이 담겨있는 배열 arr 가 주어질 때,
+// arr 의 원소들을 순서대로 이어 붙인 문자열을 return
+
+function solution(arr) {
+   var answer = '';
+   return answer;
+}
+
+// 내가 쓴 풀이
+function solution(arr) {
+   return arr.join('');
+}
+
+// 참고
+// join -> 배열의 요소들을 구분자 없이 전부 이어 붙여서 문자열로 반환
