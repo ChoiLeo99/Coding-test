ffd1d9595e26cd8986dc47eb98b6fab28b4dd02c
ChoiLeo99 <choileo0910@gmail.com>
2026-02-14 22:40:26 +0900

    프로그래머스 Lv0 문자열 곱하기 문제 풀이

---
+++ Lv0/문자열 곱하기.js
@@ -0,0 +1,18 @@
+// 문제
+// 문자열 my_string 과 정수 k 가 주어질 때,
+// my_string 을 k 번 번복한 문자열을 return
+
+function solution(my_string, k) {
+   var answer = '';
+   return answer;
+}
+
+// 내가 쓴 풀이
+function solution(my_string, k) {
+   var answer = my_string.repeat(k);
+   return answer;
+}
+
+// 풀이
+// 매개변수로 주어진 my_string 을
+// repeat -> 문자열을 반복해주는 함수로 k 번 반복후 return
