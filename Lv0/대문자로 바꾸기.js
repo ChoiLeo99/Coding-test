8a80d21bef1675d9a032885f191bbbc356c9fa8e
ChoiLeo99 <choileo0910@gmail.com>
2026-02-15 23:16:59 +0900

    프로그래머스 Lv0 대문자로 바꾸기 문제 풀이

---
+++ Lv0/대문자로 바꾸기.js
@@ -0,0 +1,18 @@
+// 문제
+// 알파벳으로 이루어진 문자열 myString 이 주어집니다.
+// 모든 알파벳을 대문자로 변환하여 return
+
+function solution(myString) {
+   var answer = '';
+   return answer;
+}
+
+// 내가 쓴 풀이
+function solution(myString) {
+   var answer = myString.toUpperCase();
+   return answer;
+}
+
+// 풀이
+// toUpperCase -> 대문자로 변환하는 메서드를 사용하여
+// 매개변수로 주어진 myString 을 return
